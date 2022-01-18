e1eecb7c6c4817b78c7d4f9b79a4adb34ad2e209
lauretteroy <laurieroy.dev@gmail.com>
2022-01-17 18:26:25 -0800

    Add Todos component

---
+++ src/components/Todos.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span><span class="incomplete-box">Incomplete</span></span>
+      <span><span class="complete">Complete</span></span>
+    </div>
+    <div class="todos">
+      <div
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        v-bind:class="{ 'is-complete': todo.complete }"
+      >
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  methods: {
+    // ...mapActions(["fetchTodos", "deleteTOdo", "updateTodo"]),
+    onDoubleClick(currentTodo) {
+      const updatedTodo = {
+        id: currentTodo.id,
+        title: currentTodo.title,
+        complete: !currentTodo.complete,
+      };
+      this.updatedTodo(updatedTodo);
+    },
+  },
+  computed: {
+    // ...mapGetters(["allTodos"]),
+  },
+  created() {
+    this.fetchTodos();
+  },
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b882;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
